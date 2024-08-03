ceaec8068599dfd6f8fb9811923ec1fe7d4d26c0
Arif Topcu <09.arif@gmail.com>
2024-08-03 18:25:38 -0400

    add deployment scripts

---
+++ deployment/start-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "--Stopping all pm2 processes..."
+pm2 delete all
+
+apps="DJ-Pasha_InternetCafe DJ-Pasha_BizsRemoteTTRPG"
+
+#Back up
+for app in $apps
+do
+	echo "--Backing up $app..."
+	cp $app/app.db /mnt/external-drive/DJ-Pasha-Admin/backup/app.db.$app
+	echo "--Backing up $app done."
+	echo "--Pulling $app..."
+	git -C $app pull --ff
+	echo "--Pulling $app done."
+    echo "--Install npm packages $app..."
+	npm --prefix $app install
+    echo "--Install npm packages $app done."
+	echo "--Building project $app..."
+	npm --prefix $app run-script build
+	echo "--Building project $app done."
+done
+echo "--Done."
+
+pm2 start;
